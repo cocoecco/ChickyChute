2c8d44a730497b94360ef3daa218dfb61f71383d
Shachar Udi <Shacharudi9@SUMacBook-Pro.local>
2012-04-26 14:03:51 -0700

    Initial Commit

---
+++ ChickyChute/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ChickyChute
+//
+//  Created by Shachar Udi on 4/26/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
